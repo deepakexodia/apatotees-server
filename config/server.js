93ecc32321393c39fa6a2530a164cff2b37790a9
deepaksingh <deepak17.singh@paytm.com>
2019-05-25 22:44:15 +0530

    made store and api ports different

--- config/server.js
+++ config/server.js
@@ -18,13 +18,13 @@ const dbUrl =
 const domain = process.env.URL || 'apatotees.herokuapp.com';
 
 module.exports = {
-	apiListenPort: process.env.PORT || 3001,
+	apiListenPort: process.env.PORT + 1 || 3001,
 	storeListenPort: process.env.PORT || 3000,
 	// used by Store (server side)
-	apiBaseUrl: `${domain}:${process.env.PORT || 3001}/api/v1`,
+	apiBaseUrl: `${domain}:${process.env.PORT + 1 || 3001}/api/v1`,
 
 	// used by Store (server and client side)
-	ajaxBaseUrl: `${domain}:${process.env.PORT || 3001}/ajax`,
+	ajaxBaseUrl: `${domain}:${process.env.PORT + 1 || 3001}/ajax`,
 
 	// Access-Control-Allow-Origin
 	storeBaseUrl: `${domain}:${process.env.PORT || 3000}`,
@@ -68,5 +68,5 @@ module.exports = {
 	// used by API
 	orderStartNumber: 1000,
 
-	developerMode: false
+	developerMode: true
 };
