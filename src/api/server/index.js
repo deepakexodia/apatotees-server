4963a50cdb2b47900c250d945dbc792ed9329453
deepaksingh <deepak17.singh@paytm.com>
2019-05-26 22:43:05 +0530

    resolving cors issue

--- src/api/server/index.js
+++ src/api/server/index.js
@@ -20,7 +20,8 @@ app.all('*', (req, res, next) => {
 	// CORS headers
 	res.header(
 		'Access-Control-Allow-Origin',
-		security.getAccessControlAllowOrigin()
+		// security.getAccessControlAllowOrigin()
+		req.header('Origin')
 	);
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Credentials', 'true');
